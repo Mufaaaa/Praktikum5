3528603dcd1126c219bc358c0a74efe50e9fc142
Mufaaaa <faiq22836@gmail.com>
2025-04-16 19:01:28 +0700

    first commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <img src="{{ asset ('img/baju.jpg')}}" alt="baju">
+    <img src="{{ asset ('img/baju1.jpg')}}" alt="baju1">
+
+</body>
+</html>
