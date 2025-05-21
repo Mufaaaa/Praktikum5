084c23cb359c915b6f0ab2dd641d4f7cebdf202b
Mufaaaa <faiq22836@gmail.com>
2025-05-21 15:36:45 +0700

    Update praktikum 8

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ListProdukController;
 
 Route::get('/', function () {
     return view('test');
@@ -12,3 +13,5 @@ Route::get('/listproduk', [ProductController::class, 'listproduk']);
 Route::get('/home', function () {
     return view('pages.home');
 });
+
+Route::get('/produk', [ListProdukController::class, 'show']);
